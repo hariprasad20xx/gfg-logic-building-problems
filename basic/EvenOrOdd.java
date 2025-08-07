5ddb0d8b8e4c21c919c0a6115460fcda5264730b
Hariprasad <hariprasadjnv963@gmail.com>
2025-08-07 10:55:30 +0530

    Create EvenOrOdd.java

---
+++ basic/EvenOrOdd.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+		if(isEven(number)) {
+            System.out.println("The number " + number + " is even.");
+        } else {
+            System.out.println("The number " + number + " is odd.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isEven(int n) {
+        if((n & 1) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+            
+    }
+}
