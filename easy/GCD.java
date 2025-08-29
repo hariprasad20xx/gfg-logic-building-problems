2418b48e88c7c916d779dc688f8144bcad994036
Hariprasad <hariprasadjnv963@gmail.com>
2025-08-29 12:55:32 +0530

    Update GCD.java

--- easy/GCD.java
+++ easy/GCD.java
@@ -2,13 +2,7 @@ import java.util.Scanner;
 
 public class GCD {
     public static int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-
-        return a;
+        return (b == 0) ? a : gcd(b, a % b);
     }
 
     public static void main(String[] args) {
